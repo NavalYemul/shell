b93bbfb36e9dff72ce5d2f56c6c455dac4ed0164
NavalYemul <NavalYemul>
2024-01-09 09:42:47 +0000

    new file

---
+++ (Clone) Getting started.py
@@ -0,0 +1,22 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC #Getting started with Databricks Notebook (with PySpark)
+# MAGIC
+
+# COMMAND ----------
+
+print("Run python")
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC println("Run scala")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC Select "RUN SQL"
+
+# COMMAND ----------
+
+
