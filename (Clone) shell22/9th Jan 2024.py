4d3b85aeeb73b80cd5b2b018c7f5320ddf7b455b
NavalYemul <NavalYemul>
2024-01-09 09:33:24 +0000

    day2

---
+++ (Clone) shell22/9th Jan 2024.py
@@ -0,0 +1,20 @@
+# Databricks notebook source
+print("Hello world")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Getting started with spark
+# MAGIC ## Databricks
+# MAGIC ### Delta lake
+# MAGIC ###### Magic command
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC -- select "RUN SQL"
+
+# COMMAND ----------
+
+
