08065455ff14575ff056f400284d1f2927cbb98c
NavalYemul <NavalYemul>
2024-01-09 10:10:43 +0000

    added sub heading

---
+++ Day2 - Clone by NGP/NGP Notebook 2 Day2  2024-01-09 09:41:34.py
@@ -0,0 +1,15 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # HEADER NGP
+# MAGIC ## Header 2 - This is a sub heading
+# MAGIC
+# MAGIC
+
+# COMMAND ----------
+
+dbutils.fs.help()
+
+
+# COMMAND ----------
+
+
