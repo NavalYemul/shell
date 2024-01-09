29a2aee7fe2641b5233623d238823bf1a85e6336
NavalYemul <NavalYemul>
2024-01-09 09:33:40 +0000

    Adding Day2 file

---
+++ (Clone) Day 2/Getting Started.py
@@ -0,0 +1,24 @@
+# Databricks notebook source
+print("Run Python")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #Getting Started with Spark
+# MAGIC ##Databricks
+# MAGIC ###Delta Lake
+# MAGIC #####Magic Commands
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC println("RUN SCALA")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT "RUN SQL"
+
+# COMMAND ----------
+
+
