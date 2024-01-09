324dc633a621d02793a69e3f5d33cefaaa92cb83
NavalYemul <NavalYemul>
2024-01-09 09:33:58 +0000

    Committing first change

---
+++ Databricks Training Day 2/Getting Started.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+# MAGIC %md 
+# MAGIC
+# MAGIC #Getting Started with Databricks Training
+# MAGIC ##Learning How to Write Markdrop text
+# MAGIC ###Databricks Training
+# MAGIC
+# MAGIC This is the first tutorial on databricks
+
+# COMMAND ----------
+
+
